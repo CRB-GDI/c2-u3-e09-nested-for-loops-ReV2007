6ab5286b3a1eeaa5e728799ebb1182ecb4f31811
ReV2007 <145356412+ReV2007@users.noreply.github.com>
2024-02-28 10:36:59 -0500

    fixedalil

---
+++ exercise9/exercise9/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int starwidth = 0;
+            int starheight = 0;
+            
+
+            Console.Write("Pick a width, any width ,':) ");
+            int widthnumberpicked = int.Parse(Console.ReadLine());
+
+            Console.Write("Pick a height, any height ,':) ");
+            int heightnumberpicked = int.Parse(Console.ReadLine());
+
+          
+
+            for (starheight = 0; starheight <= heightnumberpicked; starheight++) 
+            {
+                Console.Write( "*" );
+
+                for (starwidth = 0; starwidth <= widthnumberpicked; starwidth++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+          
+        }
+    }
+}
