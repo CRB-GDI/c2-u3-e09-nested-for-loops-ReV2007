3c2a7f727b2b2dcfdd59a1790cba7872b267573c
ReV2007 <145356412+ReV2007@users.noreply.github.com>
2024-02-29 10:28:40 -0500

    done

--- exercise9/exercise9/Program.cs
+++ exercise9/exercise9/Program.cs
@@ -4,9 +4,14 @@
     {
         static void Main(string[] args)
         {
+            // Variables 
+
             int starwidth = 0;
             int starheight = 0;
-            
+            int starwidth2 = 0;
+            int starrow = starwidth2;
+
+            // Prompts for * width and length
 
             Console.Write("Pick a width, any width ,':) ");
             int widthnumberpicked = int.Parse(Console.ReadLine());
@@ -14,20 +19,47 @@
             Console.Write("Pick a height, any height ,':) ");
             int heightnumberpicked = int.Parse(Console.ReadLine());
 
-          
+            // Prints * pattern
 
-            for (starheight = 0; starheight <= heightnumberpicked; starheight++) 
+            for (starheight = 1; starheight < heightnumberpicked; starheight++)
             {
-                Console.Write( "*" );
+                Console.Write("*");
 
-                for (starwidth = 0; starwidth <= widthnumberpicked; starwidth++)
+                for (starwidth = 1; starwidth < widthnumberpicked; starwidth++)
                 {
                     Console.Write("*");
 
                 }
                 Console.WriteLine();
             }
-          
+
+            // Prompts for secondary * parttern width
+
+            Console.Write("Pick width 2, any width ,':) ");
+            int widthnumberpicked2 = int.Parse(Console.ReadLine());
+
+
+
+            // Prints secondary * pattern
+
+            for (starrow = 0; starrow < widthnumberpicked2; starrow++)
+            {
+
+
+
+                for (starwidth2 = 0; starwidth2 <= starrow; starwidth2++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+
+
+
+
+            }
+
+
         }
     }
 }
---
+++ r/r/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Design.Serialization;
+
+namespace r
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            int length = 2;
+            for (count = 7; count > length; count = count -1)
+            {
+                Console.Write(count);
+              
+            }
+        }
+    }
+}
